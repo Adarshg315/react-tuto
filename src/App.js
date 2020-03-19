60932f87d69ad5c6e854132f47dd0bb878a768b1
Adarsh Goyal <adarshg315@gmail.com>
2020-03-19 22:01:39 +0530

    my commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Component1 from './functional/component1'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 0
+  }
+
+  increment = () => {
+    this.setState({ counter: 5 })
+  }
+  render() {
+    return (
+      <div className="App">
+        <Component1 name="Adarsh" />
+        
+        {/* <button onClick={() => this.increment()}>Button</button> */}
+        {/* <div>
+          Div 1 {this.state.counter }
+        </div>
+        <div>
+          Div 2
+        </div>
+        <div>
+          Div 3
+        </div> */}
+
+
+
+        {/* <div style={styles.styles}>
+          style
+        </div>
+        <button>Button 1</button>
+        React
+        {console.log(<div />)} */}
+
+
+        {/* {React.createElement(
+          'button',
+          { className: 'App' },
+          "React"
+        )} */}
+
+      </div>
+    );
+  }
 }
+
+// const Appinstance = new App()
+// console.log(Appinstance)
 
 export default App;
